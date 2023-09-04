089a5eb26086a473e64bc60a57c69c1f4a66b773
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-09-04 19:36:37 +0200

    added sheet component, fixed mobile view

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,37 +1,92 @@
 import Link from "next/link";
 import NavLink from "./navLink";
 import ThemeToggle from "./toggleTheme";
-import { Github, Linkedin } from "lucide-react";
+import { FolderKanban, Github, Linkedin, Menu } from "lucide-react";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
 
 const Header = () => {
     return (
-        <nav className="flex w-full border-b">
-            <div className="flex basis-1/3 gap-2 border-l p-8 ">Logo</div>
-            <div className="hidden basis-1/3 items-center justify-center gap-8 border-l p-8 lg:flex">
-                <NavLink href="/">Home</NavLink>
-                <NavLink href="/about">About</NavLink>
-                <NavLink href="/projects">Projects</NavLink>
+        <nav className="flex w-full justify-between border-b">
+            <div className="flex basis-1/3 gap-2 border-l px-8 py-4 lg:py-8">
+                <FolderKanban className="lg:h-[40px] lg:w-[40px] " />
             </div>
-            <div className="hidden flex-grow items-center justify-center gap-8 border-l p-8 lg:flex">
-                <Link
-                    href="#"
-                    className="text-muted-foreground transition-colors hover:text-foreground"
-                >
-                    <Github />
-                </Link>
-                <Link
-                    href="#"
-                    className="text-muted-foreground transition-colors hover:text-foreground"
-                >
-                    <Linkedin />
-                </Link>
-                <ThemeToggle />
+            <div className="hidden basis-2/3 lg:flex">
+                <div className="flex basis-1/2 items-center justify-center gap-16 border-l p-8">
+                    <NavLink href="/">Home</NavLink>
+                    <NavLink href="/about">About</NavLink>
+                    <NavLink href="/projects">Projects</NavLink>
+                </div>
+                <div className="flex flex-grow items-center justify-center gap-8 border-l p-8">
+                    <Link
+                        href="#"
+                        className="text-muted-foreground transition-colors hover:text-foreground"
+                    >
+                        <Github />
+                    </Link>
+                    <Link
+                        href="#"
+                        className="text-muted-foreground transition-colors hover:text-foreground"
+                    >
+                        <Linkedin />
+                    </Link>
+                    <ThemeToggle />
+                </div>
+                <div className="flex flex-grow items-center justify-center gap-2 border-l p-8">
+                    <Link href="#" className="whitespace-nowrap font-bold uppercase">
+                        Get in touch
+                    </Link>
+                </div>
             </div>
-            <div className=" flex flex-grow items-center justify-center gap-2 border-l p-8">
-                <Link href="#" className="whitespace-nowrap font-bold uppercase">
-                    Get in touch
-                </Link>
-            </div>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <button className="flex items-center justify-center gap-2 border-l px-8 py-4 lg:hidden">
+                        <span className="">Menu</span>
+                        <Menu />
+                    </button>
+                </SheetTrigger>
+                <SheetContent side="top" className="p-0">
+                    <SheetClose asChild>
+                        <div className="flex justify-between border-b">
+                            <div className="flex basis-1/3 gap-2 border-l px-8 py-4 lg:py-8">
+                                <FolderKanban className="lg:h-[40px] lg:w-[40px] " />
+                            </div>
+                            <button className="flex items-center justify-center gap-2 border-l px-8 py-4 lg:hidden">
+                                <span className="">Menu</span>
+                                <Menu />
+                            </button>
+                        </div>
+                    </SheetClose>
+                    <div className="">
+                        <div className="flex flex-col items-center gap-8 border-b p-8 text-6xl">
+                            <NavLink href="/">Home</NavLink>
+                            <NavLink href="/about">About</NavLink>
+                            <NavLink href="/projects">Projects</NavLink>
+                        </div>
+                        <div className="flex flex-grow items-center justify-center gap-16 border-b p-8">
+                            <Link
+                                href="#"
+                                className="text-muted-foreground transition-colors hover:text-foreground"
+                            >
+                                <Github className="h-16 w-16" />
+                            </Link>
+                            <Link
+                                href="#"
+                                className="text-muted-foreground transition-colors hover:text-foreground"
+                            >
+                                <Linkedin className="h-16 w-16" />
+                            </Link>
+                        </div>
+                        <div className="flex flex-grow items-center justify-center gap-2 p-8 text-3xl">
+                            <Link
+                                href="#"
+                                className="whitespace-nowrap font-bold uppercase"
+                            >
+                                Get in touch
+                            </Link>
+                        </div>
+                    </div>
+                </SheetContent>
+            </Sheet>
         </nav>
     );
 };
--- src/components/navLink.tsx
+++ src/components/navLink.tsx
@@ -5,9 +5,10 @@ import React, { type PropsWithChildren } from "react";
 import { useRouter } from "next/router";
 import { cn } from "~/lib/utils";
 
-const NavLink: React.FC<LinkProps & PropsWithChildren> = ({
+const NavLink: React.FC<LinkProps & PropsWithChildren & { className?: string }> = ({
     href,
     children,
+    className,
     ...props
 }) => {
     const router = useRouter();
@@ -19,12 +20,17 @@ const NavLink: React.FC<LinkProps & PropsWithChildren> = ({
             href={href}
             {...props}
             className={cn(
-                " font-semibold uppercase text-muted-foreground transition-colors hover:text-foreground",
+                className ?? "",
+                "font-semibold uppercase text-muted-foreground transition-colors hover:text-foreground",
                 active && " text-primary "
             )}
         >
             {children}
-            {active && <div className="h-1 w-full rounded-full bg-primary" />}
+            {active ? (
+                <div className="h-1 w-full bg-primary" />
+            ) : (
+                <div className="h-1 w-full bg-transparent" />
+            )}
         </Link>
     );
 };
