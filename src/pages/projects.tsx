6c6b23f4c95c1a6025476a9e2bf6a257265c033d
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-09-06 13:12:51 +0200

    fixed slide animation, fixed mobile view

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Montserrat } from "@next/font/google";
 import { cn } from "~/lib/utils";
 import Head from "next/head";
@@ -6,6 +6,7 @@ import { Github, LinkIcon, MoveLeft, MoveRight } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 import Img from "../../public/webstore.jpeg";
 import Link from "next/link";
+import usePrevious from "~/hooks/usePrevious";
 
 const MontserRat = Montserrat({
     subsets: ["latin"],
@@ -13,7 +14,7 @@ const MontserRat = Montserrat({
 
 interface Project {
     id: string;
-    title: string;
+    title: string[];
     description: string;
     tech: [];
     url: string;
@@ -23,8 +24,20 @@ interface Project {
 
 const Projects: Project[] = [
     {
+        id: "0",
+        title: ["Web", "Store"],
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+        earum sapiente pariatur architecto non blanditiis mollitia
+        quasi ut reiciendis dicta distinctio consequuntur, rerum
+        laboriosam quos error, facere repellendus dolore fugit.`,
+        tech: [],
+        url: Img.src,
+        githubUrl: "https://github.com/WhispBot/WebStore",
+        demoUrl: "https://web-store-psi.vercel.app/",
+    },
+    {
         id: "1",
-        title: "Web Store",
+        title: ["Web", "Store"],
         description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
         earum sapiente pariatur architecto non blanditiis mollitia
         quasi ut reiciendis dicta distinctio consequuntur, rerum
@@ -36,161 +49,141 @@ const Projects: Project[] = [
     },
     {
         id: "2",
-        title: "amet consectetur adipisicing",
+        title: ["Web", "Store"],
         description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-        earum sapiente pariatur architecto non blanditiis mollitiaquasi reiciendis dicta distinctio consequuntur, rerum
-        laboriosam quos error, facere repellendus dolore fugit.reiciendis dicta distinctio consequuntur, rerum
+        earum sapiente pariatur architecto non blanditiis mollitia
+        quasi ut reiciendis dicta distinctio consequuntur, rerum
         laboriosam quos error, facere repellendus dolore fugit.`,
         tech: [],
-        url: "https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80",
+        url: Img.src,
+        githubUrl: "https://github.com/WhispBot/WebStore",
+        demoUrl: "https://web-store-psi.vercel.app/",
     },
 ];
 
+const variants = {
+    enter: (direction: number) => ({
+        x: `${direction * 100}%`,
+    }),
+    center: { x: 0 },
+    exit: (direction: number) => ({
+        x: `${direction * -100}%`,
+    }),
+};
+
 const ProjectsPage = () => {
     const [page, setPage] = useState<number>(0);
-    const [dir, setDir] = useState("-100");
+    const [state, setState] = useState<Project | undefined>(Projects[0] ?? undefined);
+    const prev = usePrevious(page);
+    const direction = page > prev ? 1 : -1;
 
-    const handleRight = () => {
-        if (page !== Projects.length - 1) {
-            setPage(page + 1);
-            setDir("100");
-        }
-    };
-
-    const handleLeft = () => {
-        if (page > 0) {
-            setPage(page - 1);
-            setDir("-100");
-        }
-    };
-
-    // useEffect(() => {
-    //     void mainControls.start("visable");
-    // }, [state]);
+    useEffect(() => {
+        setState(Projects[page]);
+    }, [page]);
 
     return (
         <>
             <Head>
                 <title>About</title>
+                <link rel="icon" href="/logoNos.ico" />
             </Head>
             <main
                 className={cn(MontserRat.className, "relative flex-grow overflow-hidden")}
             >
-                <AnimatePresence initial={false}>
-                    {Projects.map((project, index) => {
-                        if (index === page) return;
-
-                        return (
-                            <motion.article
-                                variants={{
-                                    hidden: { opacity: 1, x: `${dir}%`, scale: 1 },
-                                    visable: { opacity: 1, x: 0, scale: 1 },
-                                    exit: { opacity: 1, x: `${dir}%`, scale: 1 },
-                                }}
-                                initial="hidden"
-                                animate="visable"
-                                exit="exit"
-                                transition={{ duration: 0.5 }}
-                                key={project.id}
-                                className="absolute left-0 top-0 grid h-full w-full grid-cols-[2fr_1fr] grid-rows-[2fr_1fr]"
-                            >
-                                <div
-                                    className="relative flex h-full items-end  gap-2 bg-cover"
-                                    style={{ backgroundImage: `url(${project?.url})` }}
+                <AnimatePresence initial={false} custom={direction}>
+                    <motion.article
+                        variants={variants}
+                        initial="enter"
+                        animate="center"
+                        exit="exit"
+                        custom={direction}
+                        transition={{ duration: 0.5 }}
+                        key={state?.id}
+                        className="absolute left-0 top-0 grid h-full w-full select-none grid-cols-1 grid-rows-[0.3fr_0.5fr_1fr_0.1fr] xl:grid-cols-[2fr_1fr] xl:grid-rows-[2fr_1fr]"
+                    >
+                        <div
+                            className="relative flex h-full items-end  gap-2 bg-cover"
+                            style={{
+                                backgroundImage: `url(${state?.url})`,
+                            }}
+                        >
+                            <div className="flex w-full border-t bg-card/50 backdrop-blur-md">
+                                <Link
+                                    href={state?.githubUrl ?? ""}
+                                    className="flex flex-grow items-center justify-center gap-1 p-4 transition-colors hover:bg-accent/50"
+                                    target="_blank"
                                 >
-                                    <div className="flex w-full border-t bg-card/50 backdrop-blur-md">
-                                        <Link
-                                            href={project.githubUrl ?? ""}
-                                            className="flex flex-grow items-center justify-center gap-1 p-4 transition-colors hover:bg-accent/50"
-                                            target="_blank"
-                                        >
-                                            <Github
-                                                size={48}
-                                                className="stroke-muted-foreground"
-                                            />
-                                        </Link>
-                                        <Link
-                                            href={project.demoUrl ?? ""}
-                                            className="flex flex-grow items-center justify-center gap-1 p-4 transition-colors hover:bg-accent/50"
-                                            target="_blank"
-                                        >
-                                            <LinkIcon
-                                                size={48}
-                                                className="text-muted-foreground "
-                                            />
-                                        </Link>
-                                    </div>
-                                </div>
-                                <div className="flex h-full flex-col justify-between border-l p-8">
-                                    <div className="space-y-4">
-                                        <h2 className="text-2xl font-semibold">
-                                            Description
-                                        </h2>
-                                        <p>{project.description}</p>
-                                    </div>
-                                    <div>
-                                        <h2 className="text-2xl font-semibold">
-                                            Tech Used
-                                        </h2>
-                                        <ul></ul>
-                                    </div>
-                                </div>
-                                <div className="flex h-full items-center overflow-hidden border-t p-8">
-                                    <motion.h1
-                                        variants={{
-                                            hidden: { opacity: 1, y: -100 },
-                                            visable: { opacity: 1, y: 0 },
-                                        }}
-                                        initial="visable"
-                                        // animate={mainControls}
-                                        transition={{ duration: 0.5 }}
-                                        className="text-6xl font-extrabold uppercase"
-                                    >
-                                        {project?.title.split(" ").map((text, index) => (
-                                            <React.Fragment key={index}>
-                                                {text}
-                                                <br />
-                                            </React.Fragment>
-                                        ))}
-                                    </motion.h1>
-                                </div>
-                                <div className="flex h-full border-l border-t">
-                                    <button
-                                        onClick={() => handleLeft()}
-                                        className="flex flex-grow items-center justify-center hover:bg-accent/50 disabled:pointer-events-none disabled:opacity-50"
-                                        disabled={page === 0}
-                                    >
-                                        <motion.div
-                                            whileHover={{ x: -25 }}
-                                            whileTap={{
-                                                scale: 0.8,
-                                            }}
-                                            transition={{ duration: 0.2 }}
-                                            className="grid h-full w-full place-content-center"
-                                        >
-                                            <MoveLeft size={96} strokeWidth="1px" />
-                                        </motion.div>
-                                    </button>
-                                    <button
-                                        onClick={() => handleRight()}
-                                        className="flex flex-grow items-center justify-center hover:bg-accent/50 disabled:pointer-events-none disabled:opacity-50"
-                                        disabled={page === Projects.length - 1}
-                                    >
-                                        <motion.div
-                                            whileHover={{ x: 25 }}
-                                            whileTap={{
-                                                scale: 0.8,
-                                            }}
-                                            transition={{ duration: 0.2 }}
-                                            className="grid h-full w-full place-content-center"
-                                        >
-                                            <MoveRight size={96} strokeWidth="1px" />
-                                        </motion.div>
-                                    </button>
-                                </div>
-                            </motion.article>
-                        );
-                    })}
+                                    <Github className="stroke-muted-foreground xl:h-12 xl:w-12" />
+                                </Link>
+                                <Link
+                                    href={state?.demoUrl ?? ""}
+                                    className="flex flex-grow items-center justify-center gap-1 p-4 transition-colors hover:bg-accent/50"
+                                    target="_blank"
+                                >
+                                    <LinkIcon className="text-muted-foreground " />
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="flex h-full flex-col justify-between border-l p-8">
+                            <div className="space-y-4">
+                                <h2 className="text-2xl font-semibold">Description</h2>
+                                <p>{state?.description}</p>
+                            </div>
+                            <div>
+                                <h2 className="text-2xl font-semibold">Tech Used</h2>
+                                <ul></ul>
+                            </div>
+                        </div>
+                        <div className="row-start-1 flex h-full items-center justify-between overflow-hidden border-t p-8 xl:row-start-auto">
+                            <h1 className="text-3xl font-extrabold uppercase xl:text-6xl">
+                                {state?.title.map((text, index) => (
+                                    <React.Fragment key={index}>
+                                        {text}
+                                        <br />
+                                    </React.Fragment>
+                                ))}
+                            </h1>
+                            <div className="flex flex-col items-center justify-center text-3xl font-extrabold uppercase xl:text-6xl">
+                                <span>{page + 1}</span>
+                                <div className="my-2 h-1 w-full bg-foreground" />
+                                <span>{Projects.length}</span>
+                            </div>
+                        </div>
+                        <div className="flex h-full border-l border-t">
+                            <button
+                                onClick={() => setPage(page - 1)}
+                                className="flex flex-grow items-center justify-center hover:bg-accent/50 disabled:pointer-events-none disabled:opacity-50"
+                                disabled={page === 0}
+                            >
+                                <motion.div
+                                    whileHover={{ x: -25 }}
+                                    whileTap={{
+                                        scale: 0.8,
+                                    }}
+                                    transition={{ duration: 0.2 }}
+                                    className="grid h-full w-full place-content-center"
+                                >
+                                    <MoveLeft size={96} strokeWidth="1px" />
+                                </motion.div>
+                            </button>
+                            <button
+                                onClick={() => setPage(page + 1)}
+                                className="flex flex-grow items-center justify-center hover:bg-accent/50 disabled:pointer-events-none disabled:opacity-50"
+                                disabled={page === Projects.length - 1}
+                            >
+                                <motion.div
+                                    whileHover={{ x: 25 }}
+                                    whileTap={{
+                                        scale: 0.8,
+                                    }}
+                                    transition={{ duration: 0.2 }}
+                                    className="grid h-full w-full place-content-center"
+                                >
+                                    <MoveRight size={96} strokeWidth="1px" />
+                                </motion.div>
+                            </button>
+                        </div>
+                    </motion.article>
                 </AnimatePresence>
             </main>
         </>
