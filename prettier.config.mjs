d63735d74900157b73f63f2d540c7a2cd44cfc9e
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-10-22 00:16:38 +0200

    added chadcn ui

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+    plugins: ["prettier-plugin-tailwindcss"],
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    printWidth: 90,
+    useTabs: false,
 };
 
 export default config;
