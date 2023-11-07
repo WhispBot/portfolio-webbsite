0156e5a7c5e7e62bf81454f2843a5904a035f6ce
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-11-07 08:45:16 +0100

    added contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+    return (
+        <main className="flex flex-grow justify-center">
+            <span className="p-16 text-5xl font-bold uppercase">Get in touch.</span>
+        </main>
+    );
+};
+
+export default Page;
