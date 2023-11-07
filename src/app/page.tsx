14b5d4ce889a9a182ce3252d585ff260e1421ba5
Whisp <46845957+WhispBot@users.noreply.github.com>
2023-11-07 08:48:55 +0100

    removed unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Upload from "./_components/upload";
-
 export default function Home() {
     return (
         <main className="flex flex-grow items-center justify-center">
-            <Upload />
             <div className="">
                 <h1 className="text-8xl font-bold">{"Hello, I'm Emil"} </h1>
                 <h1 className="text-6xl ">{"a Full-stack developer"}</h1>
